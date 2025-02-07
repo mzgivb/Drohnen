cbc4dac594b2a523a8ce399f20809abb01932368
mzgivb <jochen.leeder@mzgivb.de>
2025-02-07 13:32:01 +0100

    erster commit

--- viereck_fliegen.py
+++ viereck_fliegen.py
@@ -7,14 +7,15 @@ Batteriezustand = tello.get_battery()
 
 print ("Der Akku ist zu:" , Batteriezustand , "% geladen.")
 
-tello.move_up(30) # Tello geht auf die Höhe von 30 cm
+tello.takeoff() # Tello geht auf die Höhe von 30 cm
 
 def viereck_fliegen(kantenlaenge, radius): #Definition der Funktion viereck_fliegen
-    for x in range(4)
+    for x in range(4):
         tello.move_forward(kantenlaenge)
         tello.rotate_clockwise(radius)
 
 viereck_fliegen(50,90)
+
 
 tello.land()
 
