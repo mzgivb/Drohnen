e377a5f88627567cc88d9bb7f45d26bf57336a29
Medienzentrum Gießen Vogelsberg <jochen.leeder@mzgivb.de>
2025-02-07 21:51:45 +0100

    Update erster_flug.py

--- erster_flug.py
+++ erster_flug.py
@@ -7,7 +7,7 @@ Batteriezustand = tello.get_battery()
 
 print ("Der Akku ist zu:" , Batteriezustand , "% geladen.")
 
-tello.move_up(30) # Die Drohne habt ab
+tello.takeoff() # Die Drohne habt ab
 tello.move_forward(80)
 tello.land()
 
