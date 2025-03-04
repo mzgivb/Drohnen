0bd683937c5f08c5d2dc42dd66309519b1819ac4
mzgivb <jochen.leeder@mzgivb.de>
2025-03-04 17:06:42 +0100

    mission

---
+++ mission.py
@@ -0,0 +1,25 @@
+from djitellopy import Tello
+
+drohne = Tello()
+drohne.connect() #verbindet die Drohne mit dem Laptop
+drohne.enable_mission_pads() #aktiviert die Missionpads
+
+batterieladung = drohne.get_battery() #ermittelt die ladung der Batterie
+
+print (f"Der Akku der Drohne ist zu {batterieladung} % geladen" )
+drohne.takeoff() # Drohne startet
+
+drohne.move_forward(100) # die Drohne fliegt 100cm nach vorne
+drohne.set_video_direction(drohne.CAMERA_DOWNWARD) # die abwärtsgerichtete kamera wird eingeschaltet
+
+ID = drohne.get_mission_pad_id() # legt die ID Variable fest
+# Abfrage nach den Missionpad 
+if ID == 4:
+    print ("Ich habe das Mission Pad gefunden")
+else:
+    print(f"Aktuelle ID: {ID}")
+
+drohne.rotate_clockwise(180)
+drohne.move_forward(100)
+drohne.land()
+
